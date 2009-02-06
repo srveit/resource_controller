59c301e5d0278e550799bba1c519d085868f9c3d
Stephen R. Veit <sveit@raven.(none)>
2009-02-06 11:02:09 -0600

    Fixed titles in view templates

--- generators/scaffold_resource/templates/rspec/unit_spec.rb
+++ generators/scaffold_resource/templates/rspec/unit_spec.rb
@@ -1,11 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '<%= '/..' * controller_class_nesting_depth %>/../spec_helper')
 
 describe <%= class_name %> do
-  before(:each) do
-    @<%= file_name %> = <%= class_name %>.new
+  fixtures :<%= plural_name %>
+
+  describe "creating" do
+    before(:each) do
+      @<%= file_name %> = <%= class_name %>.new
+    end
+
+    it "should be valid" do
+      @<%= file_name %>.should be_valid
+    end
   end
 
-  it "should be valid" do
-    @<%= file_name %>.should be_valid
+  describe "existing" do
+    before(:each) do
+      @<%= file_name %> = <%= plural_name %>(:one)
+    end
+
+    it "should be valid" do
+      @<%= file_name %>.should be_valid
+    end
   end
 end
